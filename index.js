651fb36ee22641231081c6f4fa4d07df8bf30c04
Codi <123835607+Codijr1@users.noreply.github.com>
2023-02-13 19:55:26 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name:"Sam",
+    streetAddress:"11 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress){
+    return {...employee, streetAddress}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,name,streetAddress){
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(newEmployee, employee){
+    return {...employee, newEmployee};
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,newEmployee,name){
+    delete employee.name
+    return newEmployee;
+} 
